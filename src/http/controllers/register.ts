f5ccf6cd4ed872d76d91f0e3db794a20e8aa5c7a
Euclides do Rosário <euclidesdry@gmail.com>
2024-06-23 18:20:38 +0100

    feat(use-cases): ✨ adding register use-case error handler: User Email Already Exists

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 
 import { PrismaUsersRepository } from '~/repositories/prisma/prisma-users-repository'
 
+import { UserAlreadyExistsError } from '~/use-cases/errors/user-already-exists-error'
+
 import { RegisterUseCase } from '~/use-cases/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -24,7 +26,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    return reply.status(500).send()
   }
 
   return reply.code(201).send()
