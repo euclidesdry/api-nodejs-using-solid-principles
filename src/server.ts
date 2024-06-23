a5a4a383c2db305f9d7f69f8d3a25f2a2654b2f1
Euclides do Rosário <euclidesdry@gmail.com>
2024-06-23 01:04:43 +0100

    feat(app): ✨ loading env vars

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
+import { env } from "./env";
 import { app } from "./app";
 
 app
   .listen({
     host: "0.0.0.0",
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log(`🚀 HTTP Server is Running on port: \x1b[32m${3333}\x1b[0m`);
+    console.log(`🚀 HTTP Server is Running on port \x1b[32m${env.PORT}\x1b[0m`);
   });
