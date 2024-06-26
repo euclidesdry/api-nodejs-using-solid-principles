30a239612aadcbbb2379b5a66ce78ec66c56c6d3
Euclides do Rosário <euclidesdry@gmail.com>
2024-06-26 14:44:04 +0100

    test(controllers): ✅ adding register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,24 @@
-import { test } from "vitest";
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, test } from "vitest"
 
-test('OK', () => {
+import { app } from '~/app'
 
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Peixe Fresco',
+      email: 'peixe.fresco@example.com',
+      password: '123456'
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
