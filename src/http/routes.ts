56396161c7579188b1fc6c32b24bcadc5b49bb72
Euclides do Rosário <euclidesdry@gmail.com>
2024-06-26 14:59:32 +0100

    refactor(controllers): ♻️ categorizing user routes

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,9 +2,9 @@ import { FastifyInstance } from "fastify";
 
 import { verifyJWT } from "./middlewares/verify-jwt";
 
-import { authenticate } from "./controllers/authenticate";
-import { profile } from "./controllers/profile";
-import { register } from "./controllers/register";
+import { authenticate } from "./controllers/users/authenticate";
+import { profile } from "./controllers/users/profile";
+import { register } from "./controllers/users/register";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
