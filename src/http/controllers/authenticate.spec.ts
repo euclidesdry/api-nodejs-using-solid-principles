f82ef1f422f280a37953ebcf26d28630b2f008b5
Euclides do Rosário <euclidesdry@gmail.com>
2024-06-26 14:49:24 +0100

    test(controllers): ✅ adding authentication

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,5 +1,30 @@
-import { test } from "vitest";
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, test } from "vitest"
 
-test('OK', () => {
+import { app } from '~/app'
 
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('should be able to register', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Peixe Fresco',
+      email: 'peixe.fresco@example.com',
+      password: '123456'
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'peixe.fresco@example.com',
+      password: '123456'
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({ token: expect.any(String) })
+  })
 })
